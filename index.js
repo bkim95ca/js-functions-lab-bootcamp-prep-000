1996fd01fa13ca05244a5a28d7f1865c62d3c509
Brandon Kim <bkim95ca@gmail.com>
2018-02-07 01:07:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ function happyHolidays(string) {
 function happyHolidaysTo(name) {
  return `Happy holidays, ${name}!`
 }
-function happyHolidaysTo(name, holiday) {
+function happyHolidayTo(name, holiday) {
   return `Happy ${holiday}, ${name}!`
 }
